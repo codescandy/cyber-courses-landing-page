db7600b557d5ccfffb4f02b65b93a5c4e781b711
anitaparmar26 <easeanita@gmail.com>
2026-03-09 10:46:06 +0530

    updated file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "./",
+
   server: {
     host: true,
     port: 8080,
@@ -19,13 +21,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: "assets/js/[name].js",
+        entryFileNames: "assets/js/index.js",
         chunkFileNames: "assets/js/[name].js",
         assetFileNames: (assetInfo) => {
           const name = assetInfo.name ?? "";
 
           if (name.endsWith(".css")) {
-            return "assets/css/[name][extname]";
+            return "assets/css/index.css";
           }
 
           if (/\.(png|jpe?g|svg|gif|webp)$/.test(name)) {
